7ee8cfb06df76a08c6af51eee6cb12ba5a3ab347
BoberLob <75248620+Jaguar838@users.noreply.github.com>
2022-04-04 12:12:42 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,25 +1,38 @@
 const player1 = {
-  name: "Scorpion",
+  name: 'Scorpion',
   hp: 100,
-  img: "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
-  weapon: "Кинжал",
+  img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+  weapon: 'Кинжал',
   attack: function () {
     console.log(this.name + ' Fight...');
   },
 };
 
 const player2 = {
-  name: "Kitana",
+  name: 'Kitana',
   hp: 100,
-  img: "http://reactmarathon-api.herokuapp.com/assets/kitana.gif",
-  weapon: "Кинжал",
+  img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
+  weapon: 'Кинжал',
   attack: function () {
     console.log(this.name + ' Fight...');
   },
 };
+<div class='player1'>
+    <div class='progressbar'>
+        <div class='life'></div>
+        <div class='name'>SCORPION</div>
+    </div>
+    <div class='character'>
+        <img src='http://reactmarathon-api.herokuapp.com/assets/scorpion.gif' />
+    </div>
+</div>
 
+createPlayer() {
+
+}
 player1.attack();
 player2.attack();
 
-createPlayer("player1", "SCORPION", 50);
-createPlayer("player2", "KITANA", 80);
+createPlayer('player1', 'SCORPION', 50);
+createPlayer('player2', 'KITANA', 80);
+
