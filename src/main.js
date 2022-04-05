ab66e31cbf04d23686f0fdd9176a7a338732ac71
BoberLob <75248620+Jaguar838@users.noreply.github.com>
2022-04-05 13:22:49 +0300

    Update main.js
    https://www.notion.so/HomeWork-2-62f1032b84e14b84bf2027d2ed6717d4

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+const $arenas = document.querySelector('.arenas')
+
 const player1 = {
   name: 'Scorpion',
   hp: 100,
@@ -18,6 +20,41 @@ const player2 = {
   },
 };
 
+function createPlayer(playerNumber, objPlayer) {
+  const $player = document.createElement('div') // create tag div
+  $player.classList.add(playerNumber) // add class player
+
+  const  $progressbar = document.createElement('div')
+  $progressbar.classList.add('progressbar')
+  $player.appendChild($progressbar)
+
+  const  $life = document.createElement('div')
+  $life.classList.add('life')
+  $progressbar.appendChild($life)
+
+  const  $name = document.createElement('div')
+  $name.classList.add('name')
+  $name.innerText = objPlayer.name
+  $progressbar.appendChild($name)
+
+  const  $character = document.createElement('div')
+  $character.classList.add('character')
+  $player.appendChild($character)
+
+  const $img = document.createElement('img')
+  $img.src = objPlayer.img
+  $character.appendChild($img)
+
+  $arenas.appendChild($player)
+}
+
+createPlayer('player1', player1);
+createPlayer('player2', player2);
+
+player1.attack();
+player2.attack();
+
+
 // <div class='player1'>
 //     <div class='progressbar'>
 //         <div class='life'></div>
@@ -27,13 +64,3 @@ const player2 = {
 //         <img src='http://reactmarathon-api.herokuapp.com/assets/scorpion.gif' />
 //     </div>
 // </div>
-
-createPlayer() {
-
-}
-player1.attack();
-player2.attack();
-
-createPlayer('player1', 'SCORPION', 50);
-createPlayer('player2', 'KITANA', 80);
-
