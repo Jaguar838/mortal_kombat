8b3e29d30e2dd5836ab9a13935bcefb3e65ef420
BoberLob <75248620+Jaguar838@users.noreply.github.com>
2022-04-06 15:23:09 +0300

    https://www.notion.so/HomeWork-3-1c3662aa2d1f4eba9351cd633b6d7627 https://www.youtube.com/watch?v=jN9PxaHSg1w&t=1s

--- src/main.js
+++ src/main.js
@@ -1,64 +1,108 @@
-
 const $arenas = document.querySelector('.arenas')
+const $randomButton = document.querySelector('.button')
 
 const player1 = {
-  id: 1,
-  name: 'Scorpion',
-  hp: 100,
-  img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
-  weapon: 'Кинжал',
-  attack: function () {
-    console.log(this.name + ' Fight...');
-  },
+    id: 1,
+    name: 'Scorpion',
+    hp: 100,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+    weapon: 'Кинжал',
+    attack: function () {
+        console.log(this.name + ' Fight...');
+    },
 };
 
 const player2 = {
-  id: 2,
-  name: 'Kitana',
-  hp: 100,
-  img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
-  weapon: 'Кинжал',
-  attack: function () {
-    console.log(this.name + ' Fight...');
-  },
+    id: 2,
+    name: 'Kitana',
+    hp: 100,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
+    weapon: 'Кинжал',
+    attack: function () {
+        console.log(this.name + ' Fight...');
+    },
 };
-
-function createPlayer(objPlayer) {
-  const $player = createEl('div', 'player'+objPlayer.id) // create tag div and add class player
-
-  const  $progressbar = createEl('div','progressbar')
-  $player.appendChild($progressbar)
-
-  const  $life = createEl('div','life')
-  $progressbar.appendChild($life)
-
-  const  $name = createEl('div','name')
-  $name.innerText = objPlayer.name
-  $progressbar.appendChild($name)
-
-  const  $character = createEl('div','character')
-  $player.appendChild($character)
-
-  const $img = createEl('img')
-  $img.src = objPlayer.img
-  $character.appendChild($img)
-
-  return $player
-}
 
 $arenas.appendChild(createPlayer(player1));
 $arenas.appendChild(createPlayer(player2));
+
+$randomButton.addEventListener('click', function () {
+    console.log('###: Click Random Button')
+    changeHP(player1)
+    changeHP(player2)
+
+    if (player1.hp === 0 || player2.hp === 0) {
+        $randomButton.disabled = true
+    }
+
+    if (player1.hp === 0 && player1.hp < player2.hp) {
+        $arenas.appendChild(playerWins(player2.name))
+    } else if (player2.hp === 0 && player2.hp < player1.hp) {
+        $arenas.appendChild(playerWins(player1.name))
+    } else if (player1.hp ===0 && player2.hp===0) {
+        $arenas.appendChild(playerWins())
+    }
+})
 
 player1.attack();
 player2.attack();
 
 
-function createEl(tag, className) {
-  const $tag = document.createElement(tag)
-  if (className) {
-    $tag.classList.add(className)
-  }
+function changeHP(objPlayer) {
+    const $playerLife = document.querySelector('.player' + objPlayer.id + ' .life')
+    objPlayer.hp -= getRandom(20)
 
-  return $tag
+    if (objPlayer.hp <= 0) {
+        objPlayer.hp = 0
+    }
+    console.log('###: ', objPlayer.hp)
+    $playerLife.style.width = objPlayer.hp + '%'
+}
+
+function playerWins(name) {
+    const $loseTitle = createEl('div', 'loseTitle')
+    if (name) {
+        $loseTitle.innerText = name + ' wins'
+    } else {
+        $loseTitle.innerText = 'draw'
+    }
+
+    return $loseTitle
+}
+
+function createPlayer(objPlayer) {
+    const $player = createEl('div', 'player' + objPlayer.id) // create tag div and add class player
+
+    const $progressbar = createEl('div', 'progressbar')
+    $player.appendChild($progressbar)
+
+    const $life = createEl('div', 'life')
+    $progressbar.appendChild($life)
+
+    const $name = createEl('div', 'name')
+    $name.innerText = objPlayer.name
+    $progressbar.appendChild($name)
+
+    const $character = createEl('div', 'character')
+    $player.appendChild($character)
+
+    const $img = createEl('img')
+    $img.src = objPlayer.img
+    $character.appendChild($img)
+
+    return $player
+}
+
+function createEl(tag, className) {
+    const $tag = document.createElement(tag)
+    if (className) {
+        $tag.classList.add(className)
+    }
+
+    return $tag
+}
+
+function getRandom(n) {
+    return Math.ceil(Math.random() * n)
 }
 
