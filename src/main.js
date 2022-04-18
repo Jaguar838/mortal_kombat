c88f41b23dc20294b2835a38fb9157ebe0a664ff
BoberLob <75248620+Jaguar838@users.noreply.github.com>
2022-04-18 23:32:01 +0300

    Урок8 - Class что это такое и как с ним работать Классы Создаем свой класс Player()

--- src/main.js
+++ src/main.js
@@ -700,3 +700,46 @@ copy3() //{ name: 'Zar', fn: [Function: fn] }
 //************************************
 //Lesson 8 Класс и как с ним работать
 //************************************
+// В объектно-ориентированном программировании класс -
+// это расширяемый шаблон кода для создания объектов,
+// к-й устанавливает (задает в них) начальные поля:
+// какие-то значения (свойства) и реализацию поведения (методы)
+class Player {
+    constructor(props) {
+        console.log(props)
+        this.name = props.name
+        this.hp = props.hp
+        this.type = props.type
+    }
+
+    whoop = () => {
+        console.log(`${this.name} let's fight!`)
+    }
+}
+
+const player5 = new Player({
+    name: 'Scorpion',
+    hp: 100,
+    type: 'fighters'
+})
+
+console.log(player5)
+
+class Player1 extends Player {
+    constructor(props) {
+        super(props);
+        this.winners = props.winners
+    }
+
+    whoop = () => {
+        console.log('This is new WHOOP')
+    }
+}
+
+const player6 = new Player({
+    name: 'Kitana',
+    hp: 100,
+    type: 'fighters',
+    winners: true
+})
+console.log(player6)
